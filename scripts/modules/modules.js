fde450a20479a2e81564f01825bbbf2a8d1ce07b
Alfredo Alberruche <aalberrh@gmail.com>
2024-01-11 17:54:25 +0100

    Added some functions

--- scripts/modules/modules.js
+++ scripts/modules/modules.js
@@ -1,2 +1,19 @@
+export const calc_add = (num1 = null, num2 = null) => {
+  return num1 + num2;
+};
 
-// export let ---
+export const calc_substract = (num1 = null, num2 = null) => {
+  return num1 - num2;
+};
+
+export const calc_divide = (num1 = null, num2 = null) => {
+  return num1 / num2;
+};
+
+export const calc_multiply = (num1 = null, num2 = null) => {
+  return num1 * num2;
+};
+
+export const calc_square = (num1 = null) => {
+  return num1 * num1;
+};
